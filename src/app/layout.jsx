cf0ed506f3235bd69acbe86f19a1246d84aaab8f
jvstln <173433315+jvstln@users.noreply.github.com>
2024-11-05 12:43:49 +0100

    Remove Next.js default template

---
+++ src/app/layout.jsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Fleet Manager",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`antialiased`}>{children}</body>
+    </html>
+  );
+}
