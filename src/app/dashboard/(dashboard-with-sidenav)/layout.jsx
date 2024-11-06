9ef8f3072acda0ee84cdfadbcbbfa3aedfc47455
jvstln <173433315+jvstln@users.noreply.github.com>
2024-11-06 20:30:39 +0100

    Fix body height hanging

--- src/app/dashboard/(dashboard-with-sidenav)/layout.jsx
+++ src/app/dashboard/(dashboard-with-sidenav)/layout.jsx
@@ -3,7 +3,7 @@ import SideNav from "./_components/side-nav";
 
 const DashboardLayout = ({ children }) => {
   return (
-    <div className="bg-background h-screen flex p-4">
+    <div className="bg-background min-h-screen flex p-4">
       <SideNav className="text-foreground basis-1/5 font-sans text-sm" />
       <main className="p-8 rounded-s-3xl basis-4/5 grow  bg-foreground text-background overflow-auto">
         {children}
